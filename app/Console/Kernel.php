3e3776a8ac4f2f1aee30b1f43c4504353f6e0dd9
padi-dev-huept <huept@paditech.com>
2021-08-10 13:38:05 +0700

    update time runJob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,8 +28,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('crawler:post')->dailyAt('06:40');
-        $schedule->command('crawler:news')->dailyAt('00:00');
+        $schedule->command('crawler:post')->dailyAt('07:00');
+        $schedule->command('crawler:news')->dailyAt('07:00');
     }
 
     /**
